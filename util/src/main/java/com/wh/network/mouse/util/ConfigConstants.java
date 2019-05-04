0190bcadefb6f4876dbfd581a67d9faea557f6d8
superman <1187470140@qq.com>
2019-05-04 21:03:06 +0800

    零碎时间构建下系统模块的依赖

---
+++ util/src/main/java/com/wh/network/mouse/util/ConfigConstants.java
@@ -0,0 +1,8 @@
+package com.wh.network.mouse.util;
+
+public class ConfigConstants {
+
+    public static final String SERVER_DEFAULT_CONFIG = "server.json";
+    public static final String CLIENT_DEFAULT_CONFIG = "client.json";
+    public static final String USER_DEFAULT_CONFIG = "user.json";
+}
